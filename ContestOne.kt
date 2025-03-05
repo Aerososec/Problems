6c0df2d742acb0a4b7ec57d57e8d6da988deb7c3
mitia <mitia.schepetov@yandex.ru>
2025-03-05 15:22:28 +0400

    promlem one

---
+++ ContestOne.kt
@@ -0,0 +1,29 @@
+class ContestOne {
+    fun problemOne(array: Array<Int>){
+        var count = 0
+        val indicesList = mutableListOf<Pair<Int, Int>>()
+        var i = 0
+        var j = array.size - 1
+        while (i < j){
+            if (array[i] == 1 && array[j] == 0){
+                array[i] = 0
+                array[j] = 1
+                count++
+                indicesList.add(i to j)
+                i++
+                j--
+            }
+            else{
+                if (array[i] == 0)
+                    i++
+                if (array[j] == 1)
+                    j--
+            }
+        }
+        println(array.joinToString())
+        println(count)
+        for (ii in indicesList.indices step 2){
+            println("${indicesList[ii]} ${indicesList[ii+1]}")
+        }
+    }
+}
---
+++ main.kt
@@ -0,0 +1,4 @@
+fun main(){
+    val array = arrayOf(0, 1,0, 1,0, 1,0, 1,0, 1)
+    ContestOne().problemOne(array)
+}
